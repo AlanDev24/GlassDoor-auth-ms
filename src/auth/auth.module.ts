19719f9e64172bab8f1f40ff785079cf2892119f
Alan Gonzalez <alanfga1@gmail.com>
2025-01-12 15:51:57 -0700

    configure prisma module and check auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
+import { JwtModule } from '@nestjs/jwt';
+import { envs } from 'src/config';
+import { PrismaModule } from 'src/prisma';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      useFactory: () => {
+        return {
+          global: true,
+          secret: envs.jwtSecret,
+          signOptions: { expiresIn: '1h' },
+        };
+      },
+    }),
+    JwtModule,
+  ],
 })
 export class AuthModule {}
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,12 +4,14 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  JWT_SECRET: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    JWT_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -24,4 +26,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  jwtSecret: envVars.JWT_SECRET
 };
